801e9080a45ecf7b3e50438c4d712f3616372f90
Alexei Colin <ac@alexeicolin.com>
2015-05-29 16:09:49 -0400

    fw: drainer: init all pins to outputs
    This is very important because otherwise, the pins are inputs that
    are floating. Floating input lead to nonndeterministic current
    consumption: waving hands around the WISP changes the discharge time.
    This commit fixes that problem.

--- FirmwareAndInterface/drainer/src/main.asm
+++ FirmwareAndInterface/drainer/src/main.asm
@@ -7,6 +7,25 @@ main:
     ; watchdog is active on reset, so disable it, to have rogue resets
     MOV #WDTPW+WDTHOLD, &WDTCTL
 
+    ; Initialize all pins to outputs
+
+    MOV.B #0xff, &P1DIR
+    MOV.B #0x0, &P1OUT
+
+    MOV.B #0xff, &P2DIR
+    MOV.B #0x0, &P2OUT
+
+    MOV.B #0xff, &P3DIR
+    MOV.B #0x0, &P3OUT
+
+    MOV.B #0xff, &P4DIR
+    MOV.B #0x0, &P4OUT
+
+    MOV.B #0xff, &PJDIR
+    MOV.B #0x0, &PJOUT
+
+    BIC.B #LOCKLPM5, &PM5CTL0 ; "commit" pin config
+
 loop:
     ; States: LPM0 - LPM4 (change the obvious macro below)
     NOP
