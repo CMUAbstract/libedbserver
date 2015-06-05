1db0a9b5b00c306633b1bc786f0745d4ce2eeb01
Alexei Colin <ac@alexeicolin.com>
2015-06-04 21:32:54 -0400

    fw: scripts: debug enter debug mode with precharge

--- FirmwareAndInterface/scripts/debug.py
+++ FirmwareAndInterface/scripts/debug.py
@@ -6,13 +6,19 @@ mon = wispmon.WispMonitor()
 
 def main():
 
-#    target_voltage = 1.8 # default: minimum Vcc for MCU
-#
-#    if len(sys.argv) > 0:
-#        target_voltage = float(sys.argv[1])
-#
-#    print "Charging to %.4f V" % (target_voltage)
-#    mon.charge(target_voltage)
+    # TODO: think about this: at any given time, the WISP is most probably off,
+    # so it is a bit weird to have a use case that just says "enter debug mode now"
+    # precharging seems necessary in this case. Energy-break points make sense here.
+
+    # Pre-charge because to the given voltage if requested
+    if len(sys.argv) > 0:
+        target_voltage = float(sys.argv[1])
+        print "Charging to %.4f V..." % (target_voltage)
+        actual_voltage = mon.charge(target_voltage)
+        print "Charged to %.4f V" % (actual_voltage)
+
+        # WISP Boot latency (TODO: why is it taking this long?)
+        time.sleep(0.004)
 
     mon.enter_debug_mode()
 
