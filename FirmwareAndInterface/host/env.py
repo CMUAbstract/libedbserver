ee0f6ba9ef50daedbeecd423dc2e0732498aa776
Alexei Colin <ac@alexeicolin.com>
2015-08-13 10:53:18 -0400

    fw: renamed to firmware

--- FirmwareAndInterface/host/env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-CONFIG_HEADER = script_dir + '/../firmware/src/config.h'
-HOST_COMM_HEADER = script_dir + '/../firmware/src/host_comm.h'
-TARGET_COMM_HEADER = script_dir + '/../libdebug/src/include/libdebug/target_comm.h'
