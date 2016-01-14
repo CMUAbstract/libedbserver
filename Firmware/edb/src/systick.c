af1473264b7ff42074f57b8e6dcb24147c7e3232
Alexei Colin <ac@alexeicolin.com>
2016-01-14 12:07:56 -0500

    edb: systick: log messages

--- Firmware/edb/src/systick.c
+++ Firmware/edb/src/systick.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 
 #include <libmsp/periph.h>
+#include <libio/log.h>
 
 #include "config.h"
 #include "pin_assign.h"
@@ -14,6 +15,8 @@ uint32_t ticks = 0;
 
 void systick_start()
 {
+    LOG("systick: starting\r\n");
+
     // configure relative timer
     TA2CTL |= TACLR | CONFIG_TIMELOG_TIMER_SOURCE | TIMER_DIV_BITS(CONFIG_TIMELOG_TIMER_DIV);
     TA2EX0 |= TIMER_A_DIV_EX_BITS(CONFIG_TIMELOG_TIMER_DIV_EX);
