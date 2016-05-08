e77e751b059f606489c24b102f4ecbf68e32c5c5
Alexei Colin <ac@alexeicolin.com>
2016-05-08 17:21:18 -0400

    codepoint: remove pin flipping workaround
    This belongs on the libedb side, because the problem is
    with the pins routing on WISP board, not on the EDB board.

--- src/codepoint.c
+++ src/codepoint.c
@@ -77,13 +77,6 @@ static unsigned watchpoint_events_buf_idx;
 
 void set_external_breakpoint_pin_state(uint16_t bitmask, bool state)
 {
-#ifdef BOARD_EDB // flip order of codepoint pins
-    if (bitmask == 0x1)
-        bitmask = 0x2;
-    else if (bitmask == 0x2)
-        bitmask = 0x1;
-#endif // BOARD_EDB
-
     if (state)
         GPIO(PORT_CODEPOINT, OUT) |= bitmask << PIN_CODEPOINT_0;
     else
